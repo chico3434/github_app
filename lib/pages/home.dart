3a5cac0c7a2d8acb31b4c0d52f9d6e277c59c1e9
chico3434 <chicokarkow@hotmail.com>
2019-11-20 16:41:32 -0300

    Tela inicial criada e linkada com a tela de perfil

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,6 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:github_app/models/user.dart';
+import 'package:github_app/pages/profile.dart';
 import 'package:github_app/values.dart';
-import 'package:http/http.dart' as http;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -13,77 +11,13 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-Future<User> fetchUser() async {
-  final String urlApiBase = 'https://api.github.com/users/';
-  final response = await http.get(urlApiBase + Values.userName);
-  if (response.statusCode == 200) {
-    return User.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to load user');
-  }
-}
-
 class _MyHomePageState extends State<MyHomePage> {
-  Future<User> user;
+  String _userName = '';
 
-  @override
-  void initState() {
-    super.initState();
-    Values.userName = 'chico3434';
-    user = fetchUser();
-  }
-
-  void _openSearchDialog() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        String userName;
-        return Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-          elevation: 0.0,
-          backgroundColor: Colors.transparent,
-          child: Card(
-            child: Container(
-              width: 200,
-              height: 110,
-              child: Column(
-                children: <Widget>[
-                  Text("Digite o nome de usuário: "),
-                  TextFormField(
-                    onChanged: (value) {
-                      userName = value;
-                    },
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                      FlatButton(
-                        child: Text('Cancelar'),
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      FlatButton(
-                        child: Text('Buscar'),
-                        onPressed: () {
-                          setState(() {
-                            Values.userName = userName;
-                            user = fetchUser();
-                          });
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
+  void _search(userName) {
+    Values.userName = userName;
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => ProfilePage()));
   }
 
   @override
@@ -92,30 +26,36 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: FutureBuilder<User>(
-          future: user,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            } else if (snapshot.hasData) {
-              return Column(
-                children: <Widget>[
-                  ClipOval(child: Image.network(snapshot.data.avatar_url, width: 128, height: 128,),),
-                  Text(snapshot.data.name),
-                  Text(snapshot.data.bio),
-                ],
-              );
-            }
-
-            return CircularProgressIndicator();
-          },
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: Column(
+          children: <Widget>[
+            Text(
+                'Github App é um aplicativo que te permite ver os perfis do Github.'),
+            Text(
+                'Para encontrar um perfil basta digitar o nome de usuário dele abaixo: '),
+            Container(
+              margin: EdgeInsets.only(top: 5, bottom: 5),
+              child: TextField(
+                onChanged: (value) => _userName = value,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Nome de usuário',
+                  contentPadding: EdgeInsets.all(12),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              child: RaisedButton(
+                color: Colors.blue,
+                textColor: Colors.white,
+                child: Text('Buscar'),
+                onPressed: () => _search(_userName),
+              ),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.search),
-        onPressed: _openSearchDialog,
-        tooltip: 'Search',
       ),
     );
   }
---
+++ lib/pages/profile.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:github_app/models/user.dart';
+import 'package:github_app/values.dart';
+import 'package:http/http.dart' as http;
+
+Future<User> fetchUser() async {
+  final String urlApiBase = 'https://api.github.com/users/';
+  final response = await http.get(urlApiBase + Values.userName);
+  if (response.statusCode == 200) {
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load user');
+  }
+}
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  Future<User> user;
+
+  @override
+  void initState() {
+    super.initState();
+    user = fetchUser();
+  }
+
+  void _openSearchDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        String userName;
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          elevation: 2,
+          title: Text('Digite o nome de usuário: '),
+          content: TextField(
+            onChanged: (value) {
+              userName = value;
+            },
+            decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Nome de usuário', contentPadding: EdgeInsets.all(12)),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Buscar'),
+              onPressed: () {
+                setState(() {
+                  Values.userName = userName;
+                  user = fetchUser();
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: (Values.userName.isEmpty) ? Text('Github app') : Text(Values.userName),
+      ),
+      body: Center(
+        child: FutureBuilder<User>(
+          future: user,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            } else if (snapshot.hasData) {
+              return Column(
+                children: <Widget>[
+                  ClipOval(
+                    child: Image.network(
+                      snapshot.data.avatar_url,
+                      width: 128,
+                      height: 128,
+                    ),
+                  ),
+                  Text(snapshot.data.name),
+                  Text(snapshot.data.bio),
+                ],
+              );
+            }
+
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.search),
+        onPressed: _openSearchDialog,
+        tooltip: 'Search',
+      ),
+    );
+  }
+}
